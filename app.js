7531581bfd31f10f87161fc073601e63c1374e59
Ivan Mitkov <ivan.mitkov@gmail.com>
2018-09-09 12:57:33 +0300

    add login and register

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(require('express-session')({
 }));
 
 app.set('view engine', 'ejs');
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -35,6 +35,11 @@ app.use(passport.session());
 // to and from the session.
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+//auth login 
+passport.use(new LocalStrategy(User.authenticate()));
+
+//============
+//ROUTES
 
 app.get('/', (req, res) => {
     res.render('home');
@@ -43,6 +48,39 @@ app.get('/', (req, res) => {
 app.get('/secret', (req, res) => {
     res.render('secret');
 });
+
+//Auth routes
+//register routes
+app.get('/register', (req, res) => {
+    res.render('register');
+})
+app.post('/register', (req, res) => {
+    User.register(new User({ username: req.body.username }),
+        req.body.password,
+        (err, user) => {
+            if (err) {
+                console.log(err);
+                return res.redirect('/register');
+            }
+            passport.authenticate('local')(req, res, () => {
+                res.redirect('/secret');
+            })
+        });
+});
+
+//login routes
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+//middleware
+app.post('/login', passport.authenticate('local',{
+    successRedirect: '/secret',
+    failureRedirect: '/login'
+}), (req, res) => {
+
+});
+
 
 app.listen(port, process.env.IP, () => {
     console.log(`App listen to the port ${port} at IP: ${process.env.IP}`);
